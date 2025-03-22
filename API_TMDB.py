e8e6a1865638d544c14319a3b7fb5833d001060c
T-GAMER <victornadalini95@gmail.com>
2025-03-22 14:59:17 -0300

    atualizar arquivo

--- API_TMDB.py
+++ API_TMDB.py
@@ -4,7 +4,7 @@ url = "https://api.themoviedb.org/3/authentication"
 
 headers = {
     "accept": "application/json",
-    "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMWM0OTFkNWVmYTQ2NGYyY2UwN2FjZDFhNDVlMGNjZSIsIm5iZiI6MTY0NjU4MjQ0OS43OTUsInN1YiI6IjYyMjRkYWIxZTMyM2YzMDA2Y2M2MzUyMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.v0ey8wMc2MTBA3wKf9PAqOob-w869JJoDegWDmru5-0"
+    "Authorization": <chave de cebechalho>
 }
 
 response = requests.get(url, headers=headers)
