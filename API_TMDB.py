a9fc19f97fc7dae7b6f3fb71eef42181795872bd
T-GAMER <victornadalini95@gmail.com>
2025-03-29 18:36:20 -0300

    unção traduzir funcionando perfeitamente

--- API_TMDB.py
+++ API_TMDB.py
@@ -4,7 +4,7 @@ url = "https://api.themoviedb.org/3/authentication"
 
 headers = {
     "accept": "application/json",
-    "Authorization": <teste>
+    "Authorization": <chave de teste>
 }
 
 response = requests.get(url, headers=headers)
